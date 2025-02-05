1618e0d2e657763a1b3f0a5f73247c07aaff2360
Javier <leojape916@gmail.com>
2025-02-05 06:28:35 -0600

    Agregar nombres

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,13 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Array donde se guardarán los nombres
+let amigos = [];
+
+// Función que se llama cuando se hace clic en el botón
+function agregarAmigo() {
+    // Obtener el valor del input
+    const input = document.getElementById('amigo');
+    const nombre = input.value.trim(); // Eliminar espacios extra
+
+           amigos.push(nombre);
+        console.log(amigos)
+    
+}
