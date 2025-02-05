d12b5a6e5bfeb3b367624aaf2dcd5f242510515f
Javier <leojape916@gmail.com>
2025-02-05 06:35:17 -0600

    Validar Entrada

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,13 +1,22 @@
-// Array donde se guardarán los nombres
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+// Aqui se guardan los nombres
 let amigos = [];
 
 // Función que se llama cuando se hace clic en el botón
 function agregarAmigo() {
-    // Obtener el valor del input
+    // Obtenemos el valor del input
     const input = document.getElementById('amigo');
     const nombre = input.value.trim(); // Eliminar espacios extra
 
-           amigos.push(nombre);
-        console.log(amigos)
-    
+    // Si el input no está vacío, agregamos el nombre al array
+    if (nombre !== "") {
+        amigos.push(nombre);
+        console.log(amigos); 
+
+        // Limpiamos el campo de entrada xd
+        input.value = '';
+    } else {
+        alert("Por favor, ingresa un nombre.");
+    }
 }
